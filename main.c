ff63189b807c2f4a5216e274f4b7286bdb94aac5
joaomfigueiredo <joao.m.figueiredo@tecnico.ulisboa.pt>
2018-12-06 11:04:18 +0000

    singularities fixes

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main(int argc, char **argv){
     exit (0);
   char *output_name = outputName(argv[1]);
   if (output_name == NULL)
-    exit(0); 
+    exit(0);
   FILE* output = fopen(output_name, "w");
   free(output_name);
   if (output == NULL){
@@ -84,21 +84,35 @@ int main(int argc, char **argv){
       Path *path;
       Path *whole_path = NULL;
       cost_acum=0;
-      num_points = 0;
-      for(int i=0; i<num_tur_points-1; i=i+1){
-        cost = 0;
-        path = search(map, height, width, tur_points[i][0], tur_points[i][1],
-                            tur_points[i+1][0], tur_points[i+1][1], &cost, &num_points);
+      num_points=0;
+      int null_point_in_path=1;
 
-        if(!cost){
-          cost_acum=-1;
-          num_points=0;
+      for(int i=0; i<num_tur_points-1; i=i+1){
+        if(map[tur_points[i][0]][tur_points[i][1]]==0){
+          null_point_in_path=0;
           break;
         }
-        joinPaths(&whole_path, path);
+      }  
 
-        cost_acum+=cost;
-    }
+      if(null_point_in_path){
+        for(int i=0; i<num_tur_points-1; i=i+1){
+          if(tur_points[i][0] == tur_points[i+1][0] &&  tur_points[i][1] == tur_points[i+1][1])
+            continue;
+          cost = 0;
+          path = search(map, height, width, tur_points[i][0], tur_points[i][1],
+                              tur_points[i+1][0], tur_points[i+1][1], &cost, &num_points);
+
+          if(!cost){
+            cost_acum=-1;
+            num_points=0;
+            break;
+          }
+          joinPaths(&whole_path, path);
+
+          cost_acum+=cost;
+        }
+      }
+
       fprintf(output, "%d %u\n", cost_acum, num_points);
       Path *p_aux;
       while (whole_path != NULL){
